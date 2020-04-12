710eec68c54e33f2994fbbb747a8c61464d015eb
Abenezer Mamo <major.abenezer@gmail.com>
2020-04-11 20:00:11 -0700

    fixed user initialization

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -1,6 +1,6 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, \
-                  flash, g, session, redirect, url_for, abort
+    flash, g, session, redirect, url_for, abort
 # import flask login functions
 from flask_login import current_user, login_user, logout_user
 # import user model
@@ -20,7 +20,7 @@ def signin():
         email = request.form['email']
         password = request.form['password']
         # find user
-        user = User.query.filter_by(email = email).first()
+        user = User.query.filter_by(email=email).first()
         # check user has been found
         if user is None or not user.check_password(password):
             return redirect(url_for('auth.signin'))
@@ -34,6 +34,7 @@ def signin():
     flash("You need to sign in!")
     return render_template("signin.html")
 
+
 @mod_auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     if app.config['MAX_USERS_NOT_REACHED']:
@@ -42,14 +43,13 @@ def signup():
             email = request.form['email']
             password = request.form['password']
             # find user
-            user = User.query.filter_by(email = email).first()
+            user = User.query.filter_by(email=email).first()
             # check email has been found
             if user != None:
                 # return to sign up page
                 return redirect(url_for('auth.signup'))
             # initialize user
-            user = User(email=email)
-            user.set_password(password)
+            user = User(email=email, password=password)
             # add to session
             db.session.add(user)
             db.session.commit()
@@ -63,9 +63,9 @@ def signup():
                 'auth.confirm_email',
                 token=token,
                 _external=True)
-            # send the email
+            # send the emails
             send_mail(subject, app.config['MAIL_USERNAME'],
-                        [email], confirm_url)
+                      [email], confirm_url)
             # update user
             flash('Account created. Confirm your email.')
             # update user count
@@ -98,32 +98,33 @@ def confirm_email(token):
     except:
         abort(500)
 
+
 @mod_auth.route('/reset', methods=['GET', 'POST'])
 def request_reset():
-        #try:
-        if request.method == 'POST':
-            # get email from form
-            email = request.form['email']
-            # query user by emeail
-            user = User.query.filter_by(email=email).first()
-            # prepare email
-            subject = "Password reset requested"
-            # generate token
-            token = ts.dumps(user.email, salt='recover-key')
-            # build recover url
-            recover_url = url_for(
-                'auth.reset_with_token',
-                token=token,
-                _external=True)
-            # send the email
-            send_mail(subject, app.config['MAIL_USERNAME'],
-                      [email], recover_url)
-            # alert user
-            flash("Reset link sent.")
-            return redirect(url_for('base.home'))
-        return render_template("request_reset.html")
-        #except:
-        #abort(500)
+    # try:
+    if request.method == 'POST':
+        # get email from form
+        email = request.form['email']
+        # query user by emeail
+        user = User.query.filter_by(email=email).first()
+        # prepare email
+        subject = "Password reset requested"
+        # generate token
+        token = ts.dumps(user.email, salt='recover-key')
+        # build recover url
+        recover_url = url_for(
+            'auth.reset_with_token',
+            token=token,
+            _external=True)
+        # send the email
+        send_mail(subject, app.config['MAIL_USERNAME'],
+                  [email], recover_url)
+        # alert user
+        flash("Reset link sent.")
+        return redirect(url_for('base.home'))
+    return render_template("request_reset.html")
+    # except:
+    # abort(500)
 
 
 @mod_auth.route('/reset/<token>', methods=['GET', 'POST'])
@@ -149,9 +150,8 @@ def reset_with_token(token):
         return redirect(url_for('auth.signin'))
     return render_template('reset_with_token.html', token=token)
 
+
 @mod_auth.route('/signout')
 def signout():
     logout_user()
     return redirect(url_for('base.home'))
-
-
--- app/mod_auth/models.py
+++ app/mod_auth/models.py
@@ -10,11 +10,12 @@ from flask_login import UserMixin
 def load_user(id):
     return User.query.get(int(id))
 
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    email    = db.Column(db.String(128),  nullable=False, unique=True)
-    password_hash = db.Column(db.String(192),  nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128),  nullable=False, unique=True)
+    password = db.Column(db.String(192),  nullable=False)
     confirmed = db.Column(db.Boolean, default=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
@@ -23,10 +24,10 @@ class User(UserMixin, db.Model):
         self.password = generate_password_hash(password)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
