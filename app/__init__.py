f7015e01d4462edfaa85a4a3dfd7df31d84fafb1
Abenezer Mamo <contact@abmamo.com>
2020-10-06 23:04:23 -0700

    added error handling to application factory"

--- app/__init__.py
+++ app/__init__.py
@@ -31,118 +31,124 @@ ts = None
 
 
 def create_app():
-    # Define the WSGI application object
-    app = Flask(__name__)
-    # Configurations
-    app.config.from_object(config)
-    global uploads
-    # Configure the image uploading via Flask-Uploads
-    uploads = UploadSet("uploads", IMAGES)
-    configure_uploads(app, uploads)
-    global ts
-    # initialize serializer with the app secret key
-    ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
-    # import extensions
-    from app.extensions import csrf, login, mail, migrate, db
+    try:
+        # Define the WSGI application object
+        app = Flask(__name__)
+        # Configurations
+        app.config.from_object(config)
+        global uploads
+        # Configure the image uploading via Flask-Uploads
+        uploads = UploadSet("uploads", IMAGES)
+        configure_uploads(app, uploads)
+        global ts
+        # initialize serializer with the app secret key
+        ts = URLSafeTimedSerializer(app.config["SECRET_KEY"])
+        # import extensions
+        from app.extensions import csrf, login, mail, migrate, db
 
-    with app.app_context():
-        # init extensions
-        # csrf
-        csrf.init_app(app)
-        # login
-        login.init_app(app)
-        login.login_view = "auth.signin"
-        # mail
-        mail.init_app(app)
-        # db
-        db.init_app(app)
-        # migration
-        migrate.init_app(app, db)
-        # import model
-        from app.mod_auth.models import User
+        with app.app_context():
+            # init extensions
+            # csrf
+            csrf.init_app(app)
+            # login
+            login.init_app(app)
+            login.login_view = "auth.signin"
+            # mail
+            mail.init_app(app)
+            # db
+            db.init_app(app)
+            # migration
+            migrate.init_app(app, db)
+            # import model
+            from app.mod_auth.models import User
 
-        # initialise login manager
-        @login.user_loader
-        def load_user(id):
-            return User.query.get(int(id))
+            # initialise login manager
+            @login.user_loader
+            def load_user(id):
+                return User.query.get(int(id))
 
-        # Sample HTTP error handling
-        @app.errorhandler(400)
-        def not_found(error):
-            # error code
-            status_code = 400
-            # message
-            message = "bad request."
-            # return error page
-            return (
-                render_template("error.html", message=message, status_code=status_code),
-                400,
-            )
+            # Sample HTTP error handling
+            @app.errorhandler(400)
+            def not_found(error):
+                # error code
+                status_code = 400
+                # message
+                message = "bad request."
+                # return error page
+                return (
+                    render_template("error.html", message=message, status_code=status_code),
+                    400,
+                )
 
-        @app.errorhandler(404)
-        def not_found(error):
-            # error code
-            status_code = 404
-            # message
-            message = "resource not found."
-            # return error page
-            return (
-                render_template("error.html", message=message, status_code=status_code),
-                404,
-            )
+            @app.errorhandler(404)
+            def not_found(error):
+                # error code
+                status_code = 404
+                # message
+                message = "resource not found."
+                # return error page
+                return (
+                    render_template("error.html", message=message, status_code=status_code),
+                    404,
+                )
 
-        @app.errorhandler(500)
-        def server_error(error):
-            # error code
-            status_code = 500
-            # message
-            message = "internal server error."
-            # return error page
-            return (
-                render_template("error.html", message=message, status_code=status_code),
-                500,
-            )
+            @app.errorhandler(500)
+            def server_error(error):
+                # error code
+                status_code = 500
+                # message
+                message = "internal server error."
+                # return error page
+                return (
+                    render_template("error.html", message=message, status_code=status_code),
+                    500,
+                )
 
-        @app.errorhandler(502)
-        def server_error(error):
-            # error code
-            status_code = 502
-            # message
-            message = "bad gateway."
-            # return error page
-            return (
-                render_template("error.html", message=message, status_code=status_code),
-                502,
-            )
+            @app.errorhandler(502)
+            def server_error(error):
+                # error code
+                status_code = 502
+                # message
+                message = "bad gateway."
+                # return error page
+                return (
+                    render_template("error.html", message=message, status_code=status_code),
+                    502,
+                )
 
-        @app.errorhandler(CSRFError)
-        def handle_csrf_error(e):
-            return (
-                render_template("error.html", message=e.description, status_code=400),
-                400,
-            )
+            @app.errorhandler(CSRFError)
+            def handle_csrf_error(e):
+                return (
+                    render_template("error.html", message=e.description, status_code=400),
+                    400,
+                )
 
-        # date formatting
-        @app.template_filter()
-        def format_datetime(value, format="medium"):
-            if format == "full":
-                format = "EEEE, d. MMMM y 'at' HH:mm"
-            elif format == "medium":
-                format = "EE dd.MM.y HH:mm"
-            elif format == "short":
-                format = "MMM d. y"
-            return babel.dates.format_datetime(value, format)
+            # date formatting
+            @app.template_filter()
+            def format_datetime(value, format="medium"):
+                if format == "full":
+                    format = "EEEE, d. MMMM y 'at' HH:mm"
+                elif format == "medium":
+                    format = "EE dd.MM.y HH:mm"
+                elif format == "short":
+                    format = "MMM d. y"
+                return babel.dates.format_datetime(value, format)
 
-        # Import a module / component using its blueprint handler variable (mod_auth)
-        from app.mod_auth.controllers import mod_auth as auth_module
-        from app.mod_base.controllers import mod_base as base_module
-        from app.mod_cms.controllers import mod_cms as cms_module
+            # Import a module / component using its blueprint handler variable (mod_auth)
+            from app.mod_auth.controllers import mod_auth as auth_module
+            from app.mod_base.controllers import mod_base as base_module
+            from app.mod_cms.controllers import mod_cms as cms_module
 
-        # Register blueprint(s)
-        app.register_blueprint(auth_module)
-        app.register_blueprint(base_module)
-        app.register_blueprint(cms_module)
-        # create all schema
-        db.create_all()
-    # return app
-    return app
+            # Register blueprint(s)
+            app.register_blueprint(auth_module)
+            app.register_blueprint(base_module)
+            app.register_blueprint(cms_module)
+            # create all schema
+            db.create_all()
+        # return app
+        return app
+    except Exception as e:
+        # log
+        current_app.logger.warning("factory failed.")
+        # return error page
+        abort(500)
