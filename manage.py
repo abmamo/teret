24d6e73279de568b33c248534638da336fdffe7f
Abenezer Mamo <contact@abmamo.com>
2020-10-26 23:46:32 -0700

    updated migration script to use app factory

--- manage.py
+++ manage.py
@@ -1,11 +1,21 @@
 # import factory
-from app import create_app
+from app.factory import create_app
+# import db extension
 from app.extensions import db
+# migration scripts
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+# env
+from dotenv import load_dotenv
+import os
+import sys
 
+# load env
+load_dotenv()
+# get environment
+environment = os.environ.get("ENVIRONMENT")
 # create app
-app = create_app()
+app = create_app(environment=environment)
 with app.app_context():
     db.init_app(app)
     migrate = Migrate(app, db)
