1df33191bfc21e61df0d7bf7a72577df26601dff
Abenezer Mamo <contact@abmamo.com>
2020-10-27 07:13:59 -0700

    updated endpoint testing and font size for app name

--- tests/unit/test_pages.py
+++ tests/unit/test_pages.py
@@ -9,3 +9,26 @@ def test_home_page(test_client):
     """
     response = test_client.get('/')
     assert response.status_code == 200
+
+def test_signin_page(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/signin' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/signin')
+    assert response.status_code == 200
+
+def test_forgot_page(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/forgot' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/reset')
+    assert response.status_code == 200
+
+def test_nonexistent_page(test_client):
+    response = test_client.get('/get')
+    assert response.status_code == 404
+
