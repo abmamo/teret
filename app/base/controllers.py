894c967251f9112a47f79be2e99ceebad99cf7f8
Abenezer Mamo <contact@abmamo.com>
2020-10-28 14:51:11 -0700

    made each story open in a new page instead of a model

--- app/base/controllers.py
+++ app/base/controllers.py
@@ -24,3 +24,18 @@ def home():
         current_app.logger.warning("stories failed with: %s" % str(e))
         # render error page
         abort(500)
+
+@base.route("/<slug>", methods=["GET"])
+def story(slug):
+    # get story by slug
+    story = Post.query.filter_by(slug=slug).first()
+    # if story has been found
+    if story is not None:
+        # render story
+        return render_template("story.html", story=story, app_name=current_app.config["APP_NAME"])
+    # otherwise story not found in db with given slug
+    else:
+        # alert user
+        flash("story not found")
+        # redirect to homepage
+        return redirect(url_for('base.hom'))
